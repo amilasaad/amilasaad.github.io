fedba1011823fe5b2eb23d5bdaaba37d31ebe7cf
Amilbangsa Asaad <amilasaad@gmail.com>
2023-05-28 20:46:14 +0800

    Modified: Contact Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'about', component: AboutComponent},
+  { path: 'contact', component: ContactComponent},
   { path: '**', redirectTo:'/home', pathMatch: 'full'}
 ]
 
